8a87512b5ccfc076e8187f8e81cb9fff4229b6dd
Spectrum <spectrum@MacBook-Pro.local>
2016-05-07 22:11:27 +0700

    finish 2.8 problem

--- src/student/StudentApp.java
+++ src/student/StudentApp.java
@@ -1,5 +1,9 @@
 package student;
+import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.Consumer;
 
 /**
  * Display reminders of students having a birthday soon.
@@ -13,16 +17,31 @@ public class StudentApp {
 	 * @param students list of students
 	 * @param month the month to use in selecting bithdays
 	 */
-	public void filterAndPrint( List<Student> students, int month ) {
+	public void filterAndPrint( List<Student> students, Predicate<Student> filter, Consumer<Student> action, Comparator<Student> stdSort) {
+		students.sort(stdSort);
 		for(Student s : students ) {
-			if (s.getBirthdate().getMonthValue() == month)
-	                  System.out.println( s );
+			if (filter.test(s))
+	                  action.accept(s);
 		}
 	}
 	
 	public static void main(String[] args) {
+		LocalDate localDate = LocalDate.now();
 		List<Student> students = Registrar.getInstance().getStudents();
+		Predicate<Student> studentFilter = (s) -> s.getBirthdate().getMonthValue() == localDate.getMonthValue();
+		Predicate<Student> week2Filter = (s) -> s.getBirthdate().getDayOfYear() >= localDate.getDayOfYear() &&
+				s.getBirthdate().getDayOfYear() <= localDate.getDayOfYear()+14;
+		Consumer<Student> studentAction = (s) -> System.out.printf("%s %s will have birthday on %d %s. \n"
+				,s.getFirstname(),s.getLastname(),s.getBirthdate().getDayOfMonth(),localDate.getMonth());
 		StudentApp app = new StudentApp();
-		app.filterAndPrint(students, 5 /* may */);
+		Comparator<Student> nameSort = (s1,s2) -> s1.getFirstname().charAt(0) - s2.getFirstname().charAt(0);
+		Comparator<Student> birthdaySort = (s1,s2) -> s1.getBirthdate().getDayOfMonth() - s2.getBirthdate().getDayOfMonth();
+		System.out.println("Sort by name: ");
+		app.filterAndPrint(students, studentFilter, studentAction, nameSort);
+		System.out.println("\nSort by birthday: ");
+		app.filterAndPrint(students, studentFilter, studentAction, birthdaySort);
+		System.out.println("\nList of people who will have birthday in two weeks: ");
+		app.filterAndPrint(students, week2Filter, studentAction, birthdaySort);
+	
 	}
 }
